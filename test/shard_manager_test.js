dfc4fd72a2a06a8ee58184afbd3134feebcfb468
Andrei <andrei@coinmode.com>
2021-07-06 12:08:52 +0300

    :zap:

--- test/shard_manager_test.js
+++ test/shard_manager_test.js
@@ -23,6 +23,58 @@ describe("Shard Manager", function () {
 		expect(await shardManager.totalSupply()).to.equal(0)
 	})
 
+	it("only initial owner can redeem the NFT", async function () {
+		await theNFT.setDetailsAndMint(10, 3000000, 300, acc1.address)
+		await theNFT.setDetailsAndMint(11, 3000000, 3000, acc2.address)
+		expect(await theNFT.balanceOf(acc1.address, 10)).to.equal(1)
+		expect(await theNFT.balanceOf(acc2.address, 11)).to.equal(1)
+		await theNFT.connect(acc1).setApprovalForAll(shardManager.address, true)
+		await theNFT.connect(acc2).setApprovalForAll(shardManager.address, true)
+		expect(await shardManager.connect(acc1).getShards(10))
+		expect(await shardManager.connect(acc2).getShards(11))
+		expect(await shardManager.balanceOf(acc1.address)).to.equal(web3.utils.toWei("90000", "ether"))
+		expect(await shardManager.balanceOf(acc2.address)).to.equal(web3.utils.toWei("900000", "ether"))
+
+		//set allowance
+		await shardManager
+			.connect(acc1)
+			.approve(shardManager.address, web3.utils.toWei("999999999", "ether"))
+		await shardManager
+			.connect(acc2)
+			.approve(shardManager.address, web3.utils.toWei("999999999", "ether"))
+
+		//acc2 will try to get the NFT from acc1
+		await expect(shardManager.connect(acc2).getTheNFT(10)).to.be.revertedWith(
+			"only initial owner can redeem the NFT"
+		)
+
+		expect(await theNFT.balanceOf(acc2.address, 10)).to.equal(0)
+	})
+
+	it("you can't get the NFT if you don't have enough shards", async function () {
+		await theNFT.setDetailsAndMint(10, 3000000, 300, acc1.address)
+		expect(await theNFT.balanceOf(acc1.address, 10)).to.equal(1)
+		await theNFT.connect(acc1).setApprovalForAll(shardManager.address, true)
+		expect(await shardManager.connect(acc1).getShards(10))
+		expect(await shardManager.balanceOf(acc1.address)).to.equal(web3.utils.toWei("90000", "ether"))
+
+		await shardManager.connect(acc1).transfer(acc2.address, web3.utils.toWei("10000", "ether"))
+
+		expect(await shardManager.balanceOf(acc1.address)).to.equal(web3.utils.toWei("80000", "ether"))
+
+		expect(await theNFT.balanceOf(acc1.address, 10)).to.equal(0)
+
+		//set allowance
+		await shardManager
+			.connect(acc1)
+			.approve(shardManager.address, web3.utils.toWei("90000", "ether"))
+
+		await expect(shardManager.connect(acc1).getTheNFT(10)).to.be.revertedWith(
+			"burn amount exceeds balance"
+		)
+		expect(await theNFT.balanceOf(acc1.address, 10)).to.equal(0)
+	})
+
 	it("shards would be gone when you get your NFT back", async function () {
 		await theNFT.setDetailsAndMint(10, 3000000, 300, acc1.address)
 		expect(await theNFT.balanceOf(acc1.address, 10)).to.equal(1)
@@ -86,7 +138,8 @@ describe("Shard Manager", function () {
 	})
 
 	it("you must own this NFT", async function () {
-		await expect(shardManager.connect(acc1).getShards(1)).to.be.revertedWith(
+		await theNFT.setDetailsAndMint(123, 3000000, 300, acc2.address)
+		await expect(shardManager.connect(acc1).getShards(123)).to.be.revertedWith(
 			"you must own this NFT"
 		)
 	})
