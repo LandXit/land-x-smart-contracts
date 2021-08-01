0af597a5d10d398f587e6e18ae1a9a80375d8bad
x <x@to.wtf>
2021-08-01 19:18:02 +0300

    :fire:

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -1,128 +1,167 @@
 // SPDX-License-Identifier: MIT
-// pragma solidity ^0.8.6;
-// import "@openzeppelin/contracts/utils/math/SafeMath.sol";
-// import "@openzeppelin/contracts/access/Ownable.sol";
-// import "@openzeppelin/contracts/security/Pausable.sol";
-// import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-// import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-// import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
+pragma solidity ^0.8.6;
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 
-// //Staking...
-// contract Staking is Ownable, Pausable, ReentrancyGuard {
-// 	using SafeMath for uint256;
+//Staking with admin superpower
+contract Staking is Ownable, Pausable, ReentrancyGuard {
+	using SafeMath for uint256;
 
-// 	struct UserInfo {
-// 		uint256 amount; // current staked WTC
-// 		uint256 createdAt; // unix timestamp when the user created
-// 		uint256 lastUpdateAt; // unix timestamp for last details update (when pointsDebt calculated)
-// 		uint256 pointsDebt; // total points collected before latest deposit
-// 	}
+	struct UserInfo {
+		uint256 amount; // current staked WTC
+		uint256 createdAt; // unix timestamp when the user created
+		uint256 lastUpdateAt; // unix timestamp for last details update (when pointsDebt calculated)
+		uint256 pointsDebt; // total points collected before latest deposit
+	}
 
-// 	uint256 public minimumAmount = 1 * (10**18); //1 WTC
-// 	uint256 public maxAmount = 1000 * (10**18); //1000 WTC
-// 	uint256 public lockTime = 1 hours;
+	uint256 public minimumAmount = 1000 * (10**18); //1000 WTC
+	uint256 public maxAmount = 1000000 * (10**18); //1 mil WTC (per user)
+	uint256 public maximumTotalAmount = 50000000 * (10**18); //50 mil WTC (total)
+	uint256 public totalStakedAmount = 0; //counts how much users staked. (must be < maximumTotalAmount)
+	uint256 public lockTime = 1 hours;
 
-// 	//emissionRate - points generated per WTC token per second staked
-// 	uint256 public emissionRate;
+	//emissionRate - points generated per WTC token per second staked
+	uint256 public emissionRate;
 
-// 	IERC20 wtcToken; // token being staked
+	IERC20 wtcToken; // token being staked
 
-// 	mapping(address => UserInfo) public userInfo;
+	mapping(address => UserInfo) public userInfo;
 
-// 	event StakeClaimed(address user, uint256 amount);
-// 	event RewardAdded(uint256 amount);
-// 	event EmissionRateChanged(uint256 newEmissionRate);
+	event StakeClaimed(address user, uint256 amount);
+	event RewardAdded(uint256 amount);
+	event EmissionRateChanged(uint256 newEmissionRate);
 
-// 	constructor(IERC20 _lpToken, uint256 _emissionRate) {
-// 		spiToken = _lpToken;
-// 		emissionRate = _emissionRate;
-// 	}
+	constructor(IERC20 _lpToken, uint256 _emissionRate) {
+		wtcToken = _lpToken;
+		emissionRate = _emissionRate;
+	}
 
-// 	// Add reward to the smart contract (must have approval)
-// 	function addReward(uint256 _amount) external {
-// 		spiToken.transferFrom(msg.sender, address(this), _amount);
-// 		emit RewardAdded(_amount);
-// 	}
+	// Add reward to the smart contract (must have approval)
+	function addReward(uint256 _amount) external {
+		wtcToken.transferFrom(msg.sender, address(this), _amount);
+		emit RewardAdded(_amount);
+	}
 
-// 	//the core of the contract (must have approval)
-// 	function stake(uint256 _amount) external whenNotPaused nonReentrant {
-// 		require(_amount >= minimumAmount, "amount below minimumAmount");
-// 		require(_amount <= maxAmount, "amount greater than maxAmount");
-// 		require(wtcToken.transferFrom(msg.sender, address(this), _amount), "failed to transfer");
+	//the core of the contract (must have approval)
+	function stake(uint256 _amount) external whenNotPaused nonReentrant {
+		require(_amount >= minimumAmount, "amount below minimumAmount");
+		require(_amount <= maxAmount, "amount greater than maxAmount");
+		require(totalStakedAmount <= maximumTotalAmount, "this contract reached its max limit");
+		require(wtcToken.transferFrom(msg.sender, address(this), _amount), "failed to transfer");
 
-// 		// already deposited before
-// 		UserInfo storage user = userInfo[msg.sender];
-// 		if (user.amount != 0) {
-// 			user.pointsDebt = pointsBalance(msg.sender);
-// 		}
+		// already deposited before
+		UserInfo storage user = userInfo[msg.sender];
+		if (user.amount != 0) {
+			user.pointsDebt = pointsBalance(msg.sender);
+		}
 
-// 		user.amount = user.amount.add(_amount);
-// 		user.lastUpdateAt = block.timestamp;
-// 		user.createdAt = block.timestamp;
-// 	}
+		user.amount = user.amount.add(_amount);
+		user.lastUpdateAt = block.timestamp;
+		user.createdAt = block.timestamp;
 
-// 	//get them rewards
-// 	function claim() public nonReentrant {
-// 		UserInfo storage user = userInfo[msg.sender];
+		totalStakedAmount = totalStakedAmount + _amount;
+	}
 
-// 		// deduct points
-// 		uint256 amountToTransfer = pointsBalance(msg.sender);
-// 		user.pointsDebt = 0;
-// 		user.lastUpdateAt = block.timestamp;
+	//get them rewards
+	function claim() public nonReentrant {
+		UserInfo storage user = userInfo[msg.sender];
 
-// 		wtcToken.transfer(msg.sender, amountToTransfer);
-// 	}
+		// deduct points
+		uint256 amountToTransfer = pointsBalance(msg.sender);
+		user.pointsDebt = 0;
+		user.lastUpdateAt = block.timestamp;
 
-// 	function unstake() external {
-// 		UserInfo storage user = userInfo[msg.sender];
-// 		require(user.amount >= 0, "insufficient staked");
-// 		require(user.createdAt + lockTime <= block.timestamp, "tokens are locked");
+		wtcToken.transfer(msg.sender, amountToTransfer);
+	}
 
-// 		// First, send all unclaimed rewards
-// 		claim();
+	function unstake() external {
+		UserInfo storage user = userInfo[msg.sender];
+		require(user.amount >= 0, "insufficient staked");
+		require(user.createdAt + lockTime <= block.timestamp, "tokens are locked");
 
-// 		uint256 userAmount = user.amount;
-// 		user.amount = 0;
+		// First, send all unclaimed rewards
+		claim();
 
-// 		wtcToken.transfer(msg.sender, userAmount);
-// 	}
+		uint256 userAmount = user.amount;
+		user.amount = 0;
 
-// 	//calculates the undebitted points. (seconds since staked) X emission rate X amount / 10^18
-// 	function _unDebitedPoints(UserInfo memory user) internal view returns (uint256) {
-// 		return block.timestamp.sub(user.lastUpdateAt).mul(emissionRate).mul(user.amount).div(1e18);
-// 	}
+		wtcToken.transfer(msg.sender, userAmount);
+	}
 
-// 	//calculte how many points an address has
-// 	function pointsBalance(address userAddress) public view returns (uint256) {
-// 		UserInfo memory user = userInfo[userAddress];
-// 		return user.pointsDebt.add(_unDebitedPoints(user));
-// 	}
+	//forces a user to unstake (should be used with care)
+	function forceUnstake(address userAddress) external onlyOwner {
+		UserInfo storage user = userInfo[userAddress];
+		// deduct points
+		uint256 amountToTransfer = pointsBalance(userAddress);
+		user.pointsDebt = 0;
+		user.lastUpdateAt = block.timestamp;
+		wtcToken.transfer(userAddress, amountToTransfer);
+		uint256 userAmount = user.amount;
+		user.amount = 0;
+		wtcToken.transfer(userAddress, userAmount);
+	}
 
-// 	//change how many WTC per second you gain (in wei)
-// 	function changeEmissionRate(uint256 newEmissionRate) public onlyOwner {
-// 		emissionRate = newEmissionRate;
-// 		emit EmissionRateChanged(newEmissionRate);
-// 	}
+	//calculates the undebitted points. (seconds since staked) X emission rate X amount / 10^18
+	function _unDebitedPoints(UserInfo memory user) internal view returns (uint256) {
+		return block.timestamp.sub(user.lastUpdateAt).mul(emissionRate).mul(user.amount).div(1e18);
+	}
 
-// 	//owner can withdraw any token sent here. should be used with care
-// 	function reclaimToken(IERC20 token, uint256 _amount) public onlyOwner {
-// 		require(address(token) != address(0), "no 0 address");
-// 		uint256 balance = token.balanceOf(address(this));
-// 		require(_amount <= balance, "you can't withdraw more than you have");
-// 		token.transfer(msg.sender, _amount);
-// 	}
+	//calculte how many points an address has
+	function pointsBalance(address userAddress) public view returns (uint256) {
+		UserInfo memory user = userInfo[userAddress];
+		return user.pointsDebt.add(_unDebitedPoints(user));
+	}
 
-// 	function pause() public onlyOwner {
-// 		_pause();
-// 	}
+	//change how many WTC per second you gain (in wei)
+	function changeEmissionRate(uint256 newEmissionRate) external onlyOwner {
+		emissionRate = newEmissionRate;
+		emit EmissionRateChanged(newEmissionRate);
+	}
 
-// 	function unpause() public onlyOwner {
-// 		_unpause();
-// 	}
+	//change the minimum amount that this staking accepts
+	function changeMinAmount(uint256 amount) external onlyOwner {
+		minimumAmount = amount;
+	}
 
-// 	//owner can withdraw any ETH sent here
-// 	function withdraw() public onlyOwner {
-// 		uint256 balance = address(this).balance;
-// 		payable(msg.sender).transfer(balance);
-// 	}
-// }
+	//change the maximum amount that this staking accepts (per user)
+	function changeMaxAmount(uint256 amount) external onlyOwner {
+		maxAmount = amount;
+	}
+
+	//change the maximum amount that this staking accepts (globally)
+	function changeMaxTotalAmount(uint256 amount) external onlyOwner {
+		maximumTotalAmount = amount;
+	}
+
+	//change lock time of the tokens
+	function changeLockTime(uint256 newTimeInSeconds) external onlyOwner {
+		lockTime = newTimeInSeconds;
+	}
+
+	//owner can withdraw any token sent here. should be used with care
+	function reclaimToken(IERC20 token, uint256 _amount) external onlyOwner {
+		require(address(token) != address(0), "no 0 address");
+		uint256 balance = token.balanceOf(address(this));
+		require(_amount <= balance, "you can't withdraw more than you have");
+		token.transfer(msg.sender, _amount);
+	}
+
+	//blocks staking but doesn't block unstaking / claiming
+	function pause() external onlyOwner {
+		_pause();
+	}
+
+	function unpause() external onlyOwner {
+		_unpause();
+	}
+
+	//owner can withdraw any ETH sent here
+	function withdraw() external onlyOwner {
+		uint256 balance = address(this).balance;
+		payable(msg.sender).transfer(balance);
+	}
+}
--- test/staking_test.js
+++ test/staking_test.js
@@ -18,14 +18,14 @@ describe("Deploy Staking", function () {
 
 		//deploy an WTC token for
 		let WTCMockContract = await ethers.getContractFactory("ERC20Mock")
-		WTC = await WTCMockContract.deploy("ShoppingIO", "WTC", toWTC("100000"))
+		WTC = await WTCMockContract.deploy("WTC", "WTC", toWTC("99999999999999999"))
 		await WTC.deployed()
 
 		//100000000000 = 0.1 WTC per 1 WTC staked 11days
 		//10000000000 = 0.01 WTC per 1 WTC staked 11days
 		staking = await StakingC.deploy(WTC.address, "100000000000")
 		await staking.deployed()
-		await WTC.increaseAllowance(staking.address, toWTC("100000"))
+		await WTC.increaseAllowance(staking.address, toWTC("99999999999999999"))
 
 		//transfer WTC to acc1, acc2
 		await WTC.transfer(acc1.address, toWTC("1000")) //1000 WTC
@@ -38,69 +38,50 @@ describe("Deploy Staking", function () {
 		expect(await WTC.balanceOf(staking.address)).to.equal(0)
 	})
 
-	it("can add rewards", async function () {
-		expect(await WTC.balanceOf(staking.address)).to.equal(0)
-		await expect(staking.addReward(toWTC("1000"))).to.emit(staking, "RewardAdded")
-	})
-
-	it("owner can withdraw rewards", async function () {
-		await staking.addReward(toWTC("1000"))
-		//owner can't withdraw WTC
-		await expectRevert.unspecified(staking.reclaimToken(WTC.address, toWTC("300")))
-
-		//owner can withdraw OTHER tokens sent by mistake
-		let Erc20Mock = await ethers.getContractFactory("ERC20Mock")
-		let erc20 = await Erc20Mock.deploy("Shitcoin", "SHI", toWTC("100"))
-		await erc20.deployed()
-		await erc20.transfer(acc1.address, toWTC("10"))
-		await erc20.connect(acc1).increaseAllowance(staking.address, toWTC("10"))
-		await erc20.transfer(staking.address, toWTC("5"))
-		expect(await erc20.balanceOf(staking.address)).to.equal(toWTC("5"))
-		//owner can withdraw them
-		expect(await erc20.balanceOf(owner.address)).to.equal(toWTC("85"))
-		await staking.reclaimToken(erc20.address, toWTC("5"))
-		expect(await erc20.balanceOf(owner.address)).to.equal(toWTC("90"))
-	})
-
-	it("owner can change emission rate", async function () {
-		await staking.changeEmissionRate(10)
-		expect(await staking.emissionRate()).to.equal(10)
-	})
-
-	it("user can stake", async function () {
-		await staking.addReward(1000)
-		await staking.connect(acc1).stake(toWTC("1"))
-		await staking.connect(acc2).stake(toWTC("2"))
-	})
-
-	it("user can claim their rewards", async function () {
-		await staking.addReward(toWTC("1000"))
-		//stake
-		await staking.connect(acc1).stake(toWTC("1")) //1 WTC
-		await staking.connect(acc2).stake(toWTC("2")) //2 WTC
-		await time.increase(time.duration.seconds(1000000)) // ~11 days
-
-		pbAcc1 = Number(await staking.pointsBalance(acc1.address)) / 1e18
-		expect(pbAcc1).to.least(0.1)
-
-		//claim acc1
-		await staking.connect(acc1).claim()
-
-		//balance of the acc1 is now: initial (1000-1) + claimed (~0.1)
+	//acc1 starts with 1000 WTC. stakes 100. receives 50 in 1 years.
+	//total = 1150 at the end of 1 year
+	it("50% APR test", async function () {
+		await staking.changeMinAmount(1)
+		await staking.changeEmissionRate(48767800000) //50% in 1 year
+		await expect(staking.addReward(toWTC("99999999999"))).to.emit(staking, "RewardAdded")
+		await staking.connect(acc1).stake(toWTC("100")) //stake 100 WTC
+		await time.increase(time.duration.days(356))
+		await staking.connect(acc1).unstake() //unstake acc1
 		balanceAcc1 = Number(await WTC.balanceOf(acc1.address))
-		expect(balanceAcc1).to.least(Number(toWTC("999")) + Number(toWTC("0.1")))
-		expect(balanceAcc1).to.most(Number(toWTC("999")) + Number(toWTC("0.11")))
+		//balance of the acc1 is now: initial (1000) + reward (150)
+		expect(balanceAcc1).to.least(Number(toWTC("1000")) + Number(toWTC("149")))
+		expect(balanceAcc1).to.most(Number(toWTC("1000")) + Number(toWTC("151")))
+	})
 
-		//claim acc2
-		await staking.connect(acc2).claim()
+	it("users can't stake more than the total max amount", async function () {
+		await staking.changeMinAmount(1)
+		await staking.addReward(toWTC("10000"))
+		await staking.changeMaxTotalAmount(110)
+		await staking.connect(acc1).stake(toWTC("100"))
+		await expect(staking.connect(acc2).stake(toWTC("11"))).to.be.revertedWith(
+			"this contract reached its max limit"
+		)
+	})
 
-		//balance of the acc2 is now: initial (2000-2) + claimed (~0.2)
-		balanceAcc2 = Number(await WTC.balanceOf(acc2.address))
-		expect(balanceAcc2).to.least(Number(toWTC("1998")) + Number(toWTC("0.2")))
-		expect(balanceAcc2).to.most(Number(toWTC("1998")) + Number(toWTC("0.23")))
+	it("user can't unstake before the lock period", async function () {
+		await staking.changeLockTime(100)
+		await staking.changeMinAmount(1)
+		await expect(staking.addReward(toWTC("1000"))).to.emit(staking, "RewardAdded")
+
+		await staking.connect(acc1).stake(toWTC("1")) //stake 1 WTC
+
+		await time.increase(time.duration.seconds(90)) // can't unstake now
+
+		await expect(staking.connect(acc1).unstake()).to.be.revertedWith("tokens are locked")
+
+		await time.increase(time.duration.seconds(11)) // can't unstake now
+
+		await staking.connect(acc1).unstake() //all good
 	})
 
 	it("user can unstake to get everything", async function () {
+		await staking.changeMinAmount(1)
+		await staking.changeEmissionRate(100000000000) // 0.1 WTC per 1 WTC staked 11days
 		await expect(staking.addReward(toWTC("1000"))).to.emit(staking, "RewardAdded")
 
 		await staking.connect(acc1).stake(toWTC("1")) //stake 1 WTC
@@ -126,6 +107,84 @@ describe("Deploy Staking", function () {
 		leftRewards = Number(await WTC.balanceOf(staking.address))
 		expect(leftRewards).to.least(Number(toWTC("999.6"))) //- ~0.3 from previous
 		expect(leftRewards).to.most(Number(toWTC("999.7")))
+	})
+
+	it("user can claim their rewards", async function () {
+		await staking.addReward(toWTC("1000"))
+		await staking.changeMinAmount(1)
+		await staking.changeEmissionRate(100000000000) // 0.1 WTC per 1 WTC staked 11days
+		//stake
+		await staking.connect(acc1).stake(toWTC("1")) //1 WTC
+		await staking.connect(acc2).stake(toWTC("2")) //2 WTC
+		await time.increase(time.duration.seconds(1000000)) // ~11 days
+
+		pbAcc1 = Number(await staking.pointsBalance(acc1.address)) / 1e18
+		expect(pbAcc1).to.least(0.1)
+
+		//claim acc1
+		await staking.connect(acc1).claim()
+
+		//balance of the acc1 is now: initial (1000-1) + claimed (~0.1)
+		balanceAcc1 = Number(await WTC.balanceOf(acc1.address))
+		expect(balanceAcc1).to.least(Number(toWTC("999")) + Number(toWTC("0.1")))
+		expect(balanceAcc1).to.most(Number(toWTC("999")) + Number(toWTC("0.11")))
+
+		//claim acc2
+		await staking.connect(acc2).claim()
+
+		//balance of the acc2 is now: initial (2000-2) + claimed (~0.2)
+		balanceAcc2 = Number(await WTC.balanceOf(acc2.address))
+		expect(balanceAcc2).to.least(Number(toWTC("1998")) + Number(toWTC("0.2")))
+		expect(balanceAcc2).to.most(Number(toWTC("1998")) + Number(toWTC("0.23")))
+	})
+
+	it("can add rewards", async function () {
+		expect(await WTC.balanceOf(staking.address)).to.equal(0)
+		await expect(staking.addReward(toWTC("1000"))).to.emit(staking, "RewardAdded")
+	})
+
+	it("owner can withdraw OTHER tokens sent by mistake", async function () {
+		let Erc20Mock = await ethers.getContractFactory("ERC20Mock")
+		let erc20 = await Erc20Mock.deploy("Shitcoin", "shibainu", toWTC("100"))
+		await erc20.deployed()
+		await erc20.transfer(acc1.address, toWTC("10"))
+		await erc20.connect(acc1).increaseAllowance(staking.address, toWTC("10"))
+		await erc20.transfer(staking.address, toWTC("5"))
+		expect(await erc20.balanceOf(staking.address)).to.equal(toWTC("5"))
+		//owner can withdraw them
+		expect(await erc20.balanceOf(owner.address)).to.equal(toWTC("85"))
+		await staking.reclaimToken(erc20.address, toWTC("5"))
+		expect(await erc20.balanceOf(owner.address)).to.equal(toWTC("90"))
+	})
+
+	it("owner can change emission rate", async function () {
+		await staking.changeEmissionRate(10)
+		expect(await staking.emissionRate()).to.equal(10)
+	})
+
+	it("owner min/max user amounts and total max amount and lock time", async function () {
+		await staking.changeMinAmount(10)
+		expect(await staking.minimumAmount()).to.equal(10)
+
+		await staking.changeMaxAmount(100)
+		expect(await staking.maxAmount()).to.equal(100)
+
+		await staking.changeMaxTotalAmount(110)
+		expect(await staking.maximumTotalAmount()).to.equal(110)
+
+		await staking.changeLockTime(300)
+		expect(await staking.lockTime()).to.equal(300)
+
+		await expect(staking.connect(acc1).changeLockTime(300)).to.be.revertedWith(
+			"caller is not the owner"
+		)
+	})
+
+	it("user can stake", async function () {
+		await staking.changeMinAmount(1)
+		await staking.addReward(toWTC("10000"))
+		await staking.connect(acc1).stake(toWTC("1"))
+		await staking.connect(acc2).stake(toWTC("2"))
 	})
 
 	function toWTC(amount) {
